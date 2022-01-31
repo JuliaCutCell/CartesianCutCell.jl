38add5a98f443046b7f336e76808cb5dc1c7e1c0
Vincent Le Chenadec <vincentlechenadec@gmail.com>
2022-01-31 15:40:37 +0100

    ongoing work on integrating Vofinit

--- test/cylindrical.jl
+++ test/cylindrical.jl
@@ -1,10 +1,11 @@
 using LinearAlgebra
 using SparseArrays
 using CartesianCutCell
+using Vofinit
 
-bc = (dirichlet, periodic)
+bc = (periodic, periodic, periodic)
 st = stencil(bc)
-n = (3, 4)
+n = (64, 64, 64)
 
 n̅, x̅, x̅ₘ, dx̅ = mesh(bc, st, n)
 
@@ -14,5 +15,17 @@ n̅, x̅, x̅ₘ, dx̅ = mesh(bc, st, n)
 circle(x...; x₀=zero.(x), r=one(eltype(x))/2, f=identity) =
     f(norm(r)) - f(norm(x .- x₀))
 
-V̅, A̅ = integrate(circle, x̅, dx̅)
+V̅ = map(tuple.(x̅...), tuple.(dx̅...)) do x₀, h₀
+    getcelltype(circle, x₀, h₀)
+end
+
+V = restrict(V̅, n̅, n)
+
+# -1 : =0 (interface)
+# 0 : >0 (inside)
+# 1 : <0 (outside)
+cnt = count.((==).((-1, 0, 1)), Ref(V))
+cnt[2] / cnt[3] * 48 / π
+
+#V̅, A̅ = integrate(circle, x̅, dx̅)
 
